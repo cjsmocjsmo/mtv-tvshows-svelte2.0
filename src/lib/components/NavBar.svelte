e98db277b1d37e23f6a655f84da82dae06ab6093
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2025-01-28 01:54:36 -0800

    Comment out unused links in NavBar component

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -4,15 +4,15 @@
 
 <div class="fuck">
     <a href="http://10.0.4.76:8090/"><h3 class="boo">Movies</h3></a>
-    <a href="http://10.0.4.76:8091/"><h3 class="boo">TvShows</h3></a>
+    <!-- <a href="http://10.0.4.76:8091/"><h3 class="boo">TvShows</h3></a> -->
     <a href="http://10.0.4.76:9090/"><h3 class="boo">Music</h3></a>
-    <a href="http://10.0.4.76:8090/Search"><h3 class="boo">Search</h3></a>
+    <!-- <a href="http://10.0.4.76:8090/Search"><h3 class="boo">Search</h3></a> -->
 </div>
-<div class="fuck">
+<!-- <div class="fuck">
     <a href="http://10.0.4.76:8090/Propane"><h3 class="boo">Propane</h3></a>
     <a href="http://10.0.4.76:8090/Stats"><h3 class="boo">Stats</h3></a>
     <a href="http://10.0.4.76:8090/Temps"><h3 class="boo">Temps</h3></a>
-</div>
+</div> -->
 
 <style>
     .fuck {
