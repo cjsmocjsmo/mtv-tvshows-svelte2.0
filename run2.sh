9a3b5341dee702a5de7987da6243626ac05be598
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-03-24 03:38:48 -0700

    Add Dockerfiles for multiple architectures and update run script for building images

--- run2.sh
+++ run2.sh
@@ -1,87 +1,36 @@
 #!/bin/bash
 
-if ! echo "$1" | grep -qE '^[0-9]+\.[0-9]+\.[0-9]+$'; then
-    echo "The second argument must be a valid version string (e.g., 0.0.1)"
-    exit 1
-fi
+basepath="/home/cjsmo/mtv-tvshows-svelte2.0";
+builddir="$basepath/build";
 
-echo "$1";
-
-arch = uname -m;
-
-if [ "$arch" == "armv7l" ]; then
-    cp -pvr ./RPI/32/Dockerfile ./Dockerfile;
-elif [ "$arch" == "aarch64" ]; then
-    cp -pvr ./RPI/64/Dockerfile ./Dockerfile;
-elif [ "$arch" == "x86_64" ]; then
-    cp -pvr ./RPI/64/Dockerfile ./Dockerfile;
-fi
+cd $basepath;
 
 git pull https://github.com/cjsmocjsmo/mtv-tvshows-svelte2.0.git;
 
-# If all checks pass, print the arguments
+rm -rf $builddir;
 
-count1=$(echo "$1" | sed 's/\.//g' )
-count=$((count1+1-1))
-minusone=$((count-1))
+npm install;
 
-echo "Version: $1";
-echo "mtvtvshowssvelte:$1";
-echo "mtvtvshowssvelte$count";
-echo "mtvtvshowssvelte$minusone";
+npm run build;
 
-if [ "$minusone" -eq 0 ]; then
-    
-    npm install;
+arch=$(uname -m);
 
-    npm run build;
-
-    docker build -t mtvtvshowssvelte:$1 .;
-    
-    docker run --name mtvtvshowssvelte1 -d -p 8091:80 mtvtvshowssvelte:$1;
-
-    rm ./Dockerfile;
-
-    exit 0;
+if [ "$arch" = "armv7l" ]; then
+    docker build -t mtvtvshowssvelte:arm32 -f ./arm32/Dockerfile .;
+    docker run --name mtvtvshowssvelte1 -d -p 8091:80 mtvtvshowssvelte:arm32;
 fi
 
-if [ "$minusone" -eq 1 ]; then
-    # Build the Docker image
-
-    docker stop mtvtvshowssvelte1;
-
-    docker rm mtvtvshowssvelte1;
-
-    npm install;
-
-    npm run build;
-
-    docker build -t mtvtvshowssvelte:$1 .;
-
-    docker run --name mtvtvshowssvelte$count -d -p 8091:80 mtvtvshowssvelte:$2;
-
-    rm ./Dockerfile;
-
-    exit 0;
+if [ "$arch" = "aarch64" ]; then
+    docker build -t mtvtvshowssvelte:arm64 -f ./arm64/Dockerfile .;
+    docker run --name mtvtvshowssvelte1 -d -p 8091:80 mtvtvshowssvelte:arm64;
 fi
 
+if [ "$arch" = "x86_64" ]; then
+    docker build -t mtvtvshowssvelte:amd64 -f ./amd64/Dockerfile .;
+    docker run --name mtvtvshowssvelte1 -d -p 8091:80 mtvtvshowssvelte:amd64;
+fi
 
-if [ "$minusone" -gt 1 ]; then
-    # Build the Docker image
-
-    docker stop mtvtvshowssvelte$minusone;
-
-    docker rm mtvtvshowssvelte$minusone;
-
-    npm install;
-
-    npm run build;
-
-    docker build -t mtvtvshowssvelte:$1 .;
-
-    docker run --name mtvtvshowssvelte$count -d -p 8091:80 mtvtvshowssvelte:$1;
-
-    rm ./Dockerfile;
-
-    exit 0;
+if [ "$arch" = "i386" ]; then
+    echo "i386 architecture not supported";
+    exit 1;
 fi
