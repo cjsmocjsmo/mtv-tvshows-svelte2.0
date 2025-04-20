aaeae13937c22a6f686fd87b91dc3ac49bea4310
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-04-19 23:10:39 -0700

    Remove Docker images for specific architectures before building new ones in run script

--- run.sh
+++ run.sh
@@ -16,6 +16,7 @@ arch=$(uname -m);
 if [ "$arch" = "armv7l" ]; then
     docker stop mtvtvshowssvelte_arm32;
     docker rm mtvtvshowssvelte_arm32;
+    docker rmi mtvtvshowssvelte:arm32;
     docker build -t mtvtvshowssvelte:arm32 -f ./arm32/Dockerfile .;
     docker run --name mtvtvshowssvelte_arm32 -d -p 8091:80 mtvtvshowssvelte:arm32;
 fi
@@ -23,6 +24,7 @@ fi
 if [ "$arch" = "aarch64" ]; then
     docker stop mtvtvshowssvelte_arm64;
     docker rm mtvtvshowssvelte_arm64;
+    docker rmi mtvtvshowssvelte:arm64;
     docker build -t mtvtvshowssvelte:arm64 -f ./arm64/Dockerfile .;
     docker run --name mtvtvshowssvelte_arm64 -d -p 8091:80 mtvtvshowssvelte:arm64;
 fi
@@ -30,6 +32,7 @@ fi
 if [ "$arch" = "x86_64" ]; then
     docker stop mtvtvshowssvelte_amd64;
     docker rm mtvtvshowssvelte_amd64;
+    docker rmi mtvtvshowssvelte:amd64;
     docker build -t mtvtvshowssvelte:amd64 -f ./amd64/Dockerfile .;
     docker run --name mtvtvshowssvelte_amd64 -d -p 8091:80 mtvtvshowssvelte:amd64;
 fi
