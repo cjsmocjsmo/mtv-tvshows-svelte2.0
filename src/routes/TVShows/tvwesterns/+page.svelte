91b38b35faf3872ec29c4df1b86abbccaef3eed8
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-04-22 09:59:39 -0700

    Remove unused image import for 1923 in TV westerns page

--- src/routes/TVShows/tvwesterns/+page.svelte
+++ src/routes/TVShows/tvwesterns/+page.svelte
@@ -1,8 +1,6 @@
 <script>
 	import BackArrow from '$lib/components/BackArrow.svelte';
 	import TvhFord1923Card from '$lib/components/western/TVHFord1923Card.svelte';
-    import HFord1923Img from '$lib/images/western/1923.avif';
-
 </script>
 
 <div>
