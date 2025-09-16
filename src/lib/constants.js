e14344606039f2ccaf1d7a2d21e3ea9e04daba50
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2025-09-16 04:57:30 -0700

    ✨ Add Crime category and implement Tony and Ziva card component with season loading functionality

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,6 +1,7 @@
 export const TV_CATEGORIES = [
 	{ id: 'action', name: 'Action', path: '/TVShows/tvaction' },
 	{ id: 'comedy', name: 'Comedy', path: '/TVShows/tvcomedy' },
+	{ id: 'crime', name: 'Crime', path: '/TVShows/tvcrime' },
 	{ id: 'fantasy', name: 'Fantasy', path: '/TVShows/tvfantasy' },
 	{ id: 'mcu', name: 'MCU', path: '/TVShows/tvmcu' },
 	{ id: 'science', name: 'Science', path: '/TVShows/tvscience' },
---
+++ src/routes/TVShows/tvcrime/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import BackArrow from '$lib/components/BackArrow.svelte';
+	import TonyAndZiva from '$lib/components/crime/TonyAndZivaCard.svelte';
+    
+
+</script>
+
+<div>
+	<BackArrow path="/" />
+	<div class="foo">
+		<div class="bar">
+			<TonyAndZiva src={"http://10.0.4.41:9998/tonyandziva.jpg"} />
+		</div>
+	</div>
+</div>
+
+<style>
+	.bar {
+		display: grid;
+  		grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  		gap: 1rem;
+  		padding: 1rem;
+	}
+	@container (min-width: 600px) {
+  		.bar {
+			grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+		}
+	}
+</style>
