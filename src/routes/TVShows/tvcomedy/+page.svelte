1855c056707559f68adaea7a4239791fd7f69a27
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2025-10-18 01:08:23 -0700

    ✨ Refactor layout of TV comedy page to use grid display for improved responsiveness

--- src/routes/TVShows/tvcomedy/+page.svelte
+++ src/routes/TVShows/tvcomedy/+page.svelte
@@ -17,10 +17,29 @@
 
 <style>
 	.bar {
-		display: flex;
-		flex-direction: row;
-		flex-wrap: wrap;
-		justify-content: center;
-		align-items: center;
+		display: grid;
+		grid-template-columns: repeat(1, 1fr);
+		grid-gap: 1rem;
+	}
+
+	/* h1 {
+		color: yellowgreen;
+	} */
+
+	@media (min-width: 500px) {
+		.bar {
+			grid-template-columns: repeat(2, 1fr);
+		}
+	}
+	@media (min-width: 700px) {
+		.bar {
+			grid-template-columns: repeat(3, 1fr);
+		}
+	}
+	@media (min-width: 900px) {
+		.bar {
+			grid-template-columns: repeat(4, 1fr);
+		}
 	}
 </style>
+
